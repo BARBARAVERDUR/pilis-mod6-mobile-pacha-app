3e2748ae68e23362f4882c903218cff39659abeb
Barbara Verdur <barbiverdur@gmail.com>
2023-08-31 22:16:43 -0300

    Configurando Entorno

---
+++ src/screens/home/HomeScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const HomeScreen = () => {
+   <View style={styles.container}>
+     <Text>Hola bb</Text>
+   </View>;
+
+}
