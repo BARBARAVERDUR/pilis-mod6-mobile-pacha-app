932092f2f3b651a666bc3689747c350c83b02e86
Barbara Verdur <barbiverdur@gmail.com>
2023-09-20 19:50:08 -0300

    HomeScreen v1.00

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export const HomeScreen = () => {
-   <View >
-     <Text>Hola bb</Text>
-   </View>;
+export const HomeScreen = ({navigation}) => {
+
+  const [searchQuery, setSearchQuery] = useState('')
+  const [containerList, setContainerList] = useState([])
+
+  const handleSearch = (query) => {
+    setSearchQuery(query)
+  }
+  const filteredContainer = containerList.filter(container => (
+    container.title.toLowerCase().includes(searchQuery.toLowerCase())
+  ))
+
+  useEffect(() => {
+    getContainerList()
+      .then(data => {
+        setContainerList(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  const container = ({ item }) => (
+    <Pressable onPress={() => navigation.navigate('LocationDetail', { item })}>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+        <Text style={styles.itemPrice}>{item.price}</Text>
+        <Text style={styles.itemPrice}>{item.organization}</Text>
+        <Text style={styles.itemPrice}>{item.id_container_type.Container_type.title}</Text>
+      </View>
+    </Pressable>
+  )
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <SearchBar handleSearch={handleSearch} searchQuery={searchQuery} />
+      <FlatList
+        data={filteredContainer}
+        renderItem={container}
+        keyExtractor={item => item.id}
+        style={styles.itemList}
+      />
+    </SafeAreaView>
+  )
 
 }
