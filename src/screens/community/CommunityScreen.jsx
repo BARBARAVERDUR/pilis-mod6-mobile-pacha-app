ebd64c34a4544d15cb78a304ee620813e294106b
Barbara Verdur <barbiverdur@gmail.com>
2023-09-20 19:39:11 -0300

    Api connection

--- src/screens/community/CommunityScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-
-export const CommunityScreen = () => {
-  <View >
-     <Text>Hola bb desde la comunidat</Text>
-   </View>;
-
-}
